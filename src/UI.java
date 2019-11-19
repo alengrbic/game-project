6e3d2e40fa5066368a0372876f6112d1f1648335
Alen Grbic <alengrbic90@gmail.com>
2019-11-19 19:57:59 +0000

    deleted import

--- src/UI.java
+++ src/UI.java
@@ -5,8 +5,6 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 import javax.swing.*;
 import javax.swing.border.Border;
 
